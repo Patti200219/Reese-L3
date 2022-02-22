41987ca459e37828d823fc1096504332f0453119
Patti200219 <reesepattison@gmail.com>
2022-02-22 15:45:25 -0500

    inital command

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+
+module.exports = function(eleventyConfig) {
+     eleventyConfig.addPassthroughCopy('css')
+     eleventyConfig.addPassthroughCopy('img')
+     eleventyConfig.setBrowserSyncConfig({
+        files: './_site/css/**/*.css'
+        });
+     return {
+         passthroughFileCopy: true
+     }
+ }
+
+ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+};
---
+++ about.md
@@ -0,0 +1,18 @@
+---
+layout: layout.html
+title: Aboot mii
+eleventyNavigation: 
+ key: About Me 
+---
+# {{ title }} 
+     this page is aboot mii
+
+     This is the second pa
+<div class="Marvel">
+     1. Iron Man
+     2. Spider-Man 
+     3. Thor
+     4. Thanos
+     5. Hawkeye
+     6. Potato
+</div>
